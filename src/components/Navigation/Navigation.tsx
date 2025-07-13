7ccbe964e29d5c217ca91398b312580622475240
admin <admin@MacBook-Air-admin.local>
2025-07-13 23:34:25 +0300

    adding adaptability

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navigation.css';
+import menuButton from '../../images/header__menu-button.png';
 
 export default function Navigation() {
+    const [isOpen, setOpen] = useState(false);
     return (
-        <nav className="navigation__list">
-            <a href="#" className="navigation_logo">
-                Logo
-            </a>
+        <>
+            <nav className="navigation__list">
+                <a href="#" className="navigation_logo">
+                    Logo
+                </a>
 
-            <div className="navigation_link">
-                <a href="#about">About</a>
-                <a href="#work">Work</a>
-                <a href="#contact">Contact</a>
-            </div>
-        </nav>
+                <div className={`navigation_link ${isOpen ? 'active' : ''}`}>
+                    <a href="#about">About</a>
+                    <a href="#work">Work</a>
+                    <a href="#contact">Contact</a>
+                </div>
+                <button
+                    className="header__menu-button"
+                    onClick={() => setOpen(!isOpen)}
+                >
+                    <img src={menuButton} alt="" />
+                </button>
+            </nav>
+        </>
     );
 }
